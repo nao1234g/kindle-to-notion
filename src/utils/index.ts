629b5b6bc11ee728cb8bd1d4c982da42d5f69fef
nao1234g <173243159+nao1234g@users.noreply.github.com>
2025-05-01 12:31:04 +0900

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,29 @@
+/* -------------------------------------------------
+ * utils バレルファイル
+ *  - utils/common.ts で定義した I/O 系ヘルパーを再エクスポート
+ *  - utils/notion.ts で定義した Notion API 用ビルダーを再エクスポート
+ * ------------------------------------------------- */
+
+// ---------- common ヘルパー ----------
 export {
+  /* 既存ロジック */
   writeToFile,
   readFromFile,
   updateSync,
   getUnsyncedHighlights,
   formatAuthorName,
+
+  /* 追加: TXT / JSON 自動読み書き関連 */
+  readTxt,
+  readJson,
+  readAuto,
+  writeJson,
+  writeTxt,
 } from "./common";
-export { makeBlocks, makeHighlightsBlocks, makePageProperties } from "./notion";
+
+// ---------- Notion ブロック生成 ----------
+export {
+  makeBlocks,
+  makeHighlightsBlocks,
+  makePageProperties,
+} from "./notion";
